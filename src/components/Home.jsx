2db20d6b489ab4395c528f40417657ab8d4a013f
thirumurugan sivalingam <thiru82003@gmail.com>
2024-09-10 18:25:21 +0530

    asdas

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -130,7 +130,7 @@ const reff1 = setTimeout(() => {
       <div className="p-4">
         <div className=" text-base font-bold text-center p-1 bg-[#353547] w-[140px] rounded-t-xl mx-auto">
           <p className="text-[#31D0AA] text-base font-extrabold">UP</p>
-          <p className="text-[#B0A5C9] text-xs">{!!isNaN(Number(upval)) ? upval : 0 }x <span className=" font-medium"> payout</span></p>
+          <p className="text-[#B0A5C9] text-xs">{!isNaN(Number(upval)) ? upval : 0 }x <span className=" font-medium"> payout</span></p>
         </div>
         <div className="w-full mx-auto border-2 border-[#A881FC] p-4 rounded-xl">
           <div className="flex justify-between font-bold text-sm">
